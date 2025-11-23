77fcf8fdcfe08c3e3df05fc158c4fe1af90a9748
Alexander Forselius <drsounds@gmail.com>
2025-11-23 14:11:13 +0100

    Adding stitch app component from earlier own project

---
+++ plugins/apps/src/components/StitchApp/index.tsx
@@ -0,0 +1,143 @@
+import React, { useEffect, useRef, useState } from 'react';
+
+import { LoadingScreen } from '../LoadingScreen';
+import { ProgressBar } from '../ProgressBar';
+
+import styles from './index.module.css';
+import { StitchApplication } from '../../types';
+
+interface StitchAppProps {
+    uri: string;
+    onNavigate?: (path: string[]) => void;
+    onLoad?: () => void;
+    origin?: string | null;
+    defaultQuery?: any;
+    onMessage?: (action: string, data: any) => void;
+    onFetchAppById?: (appId: string) => Promise<StitchApplication>;
+    style?: React.CSSProperties;
+    [key: string]: any;
+}
+
+export function StitchApp({
+    uri,
+    onNavigate,
+    onLoad,
+    origin = null,
+    defaultQuery = {},
+    onMessage,
+    onFetchAppById,
+    style
+}: StitchAppProps) {
+    const [loaded, setLoaded] = useState(0)
+    const [app, setApp] = useState<StitchApplication | null>(null)
+    const iframeRef = useRef<HTMLIFrameElement>(null)
+    const parts = uri.split(/:/)
+    const appId = parts[2]
+    const params = parts.slice(3)
+
+    let loadingInterval: any = null
+
+    const [query, setQuery] = useState(defaultQuery)
+
+    const handleMessage = (action: string, data: any) => {
+        if (onMessage instanceof Function) {
+            onMessage(action, data);
+        }
+        if (action === 'load') {
+            handleLoad(data.value ?? 120)
+        }
+    }
+
+    const handleLoad = (value: number) => {
+        setLoaded(value)
+    }
+
+    useEffect(() => {
+        loadingInterval = setInterval(() => {
+            setLoaded((prevLoaded) => prevLoaded + 10)
+            if (loaded >= 120) {
+                clearInterval(loadingInterval)
+            }
+
+        }, 200)
+    }, [])
+    useEffect(() => {
+        onFetchAppById?.(appId).then(appResult => {
+            setApp(appResult);
+
+        })
+    }, [uri])
+    useEffect(() => {
+        if (!iframeRef?.current) return;
+        iframeRef.current.contentWindow?.postMessage({
+            params
+        }, origin ?? '*')
+    }, [iframeRef.current])
+    useEffect(() => {
+        if (!iframeRef.current) return;
+        if (typeof window !== 'undefined') {
+            window.addEventListener('message', (event) => {
+                if (event.data?.action === 'load') {
+                    if (onLoad instanceof Function) {
+                        onLoad();
+                    }
+                }
+                handleMessage(
+                    event.data.action,
+                    event.data
+                )
+            })
+        }
+    }, [iframeRef.current])
+    useEffect(() => {
+        if (!iframeRef.current) return;
+        if (typeof MutationObserver !== 'undefined') {
+            new MutationObserver((mutations) => {
+                mutations.some((mutation) => {
+                    if (mutation.type === 'attributes' && mutation.attributeName === 'src') {
+
+                        console.log('Old src: ', mutation.oldValue);
+                        // @ts-ignore
+                        console.log('New src: ', mutation.target?.src);
+                        // @ts-ignore
+                        let pathname = mutation.target?.src?.split(/\//, 3)[3]
+                        if (onNavigate instanceof Function) {
+                            onNavigate(pathname.split(/\//g))
+                        }
+                        return true;
+                    }
+
+                    return false;
+                });
+            }).observe(
+                iframeRef.current, {
+                attributes: true,
+                attributeFilter: ['src'],
+                attributeOldValue: true,
+                characterData: false,
+                characterDataOldValue: false,
+                childList: false,
+                subtree: true
+            }
+            )
+        }
+    }, [iframeRef.current])
+    if (app) {
+        return (
+            <div style={{ position: 'relative', ...style }}>
+                <iframe
+                    ref={iframeRef}
+                    src={`${[app.embed_url, ...params].join('/')}?${new URLSearchParams(query).toString()}`}
+                    allowTransparency={true}
+                    style={{ border: 'none', visibility: loaded >= 120 ? 'visible' : 'hidden', position: 'absolute', left: 0, width: '100%', height: '100vh', top: 0, flex: 1 }}
+                ></iframe>
+                {loaded <= 120 &&
+                    <div style={{ visibility: loaded <= 120 ? 'visible' : 'hidden' }} className={styles.stitchAppLoading}>
+                        <ProgressBar value={loaded} />
+                    </div>}
+            </div>
+        )
+    } else {
+        return <LoadingScreen />
+    }
+}
